b46e9168da481cd0a8b29b97e9c7b41ff7fa824d
MrAnatoliy <95148318+MrAnatoliy@users.noreply.github.com>
2023-05-11 20:13:31 +0300

    authentication from front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,39 +4,20 @@ import { useLocalState } from "./util/useLocalStorage";
 import { Routes, Route } from "react-router-dom";
 import Mainpage from "./mainpage";
 import Login from "./login";
+import PrivateRoute from "./privateRoute";
 
 function App() {
-  const [jwt, setJwt] = useLocalState("", "jwt");
-
-  // useEffect(() => {
-  //   if (!jwt) {
-  //     const reqBody = {
-  //       email: "rodionov.tolik@gmail.com",
-  //       password: "qwerty",
-  //     };
-
-  //     fetch("http://localhost:8080/api/v1/auth/authenticate", {
-  //       headers: {
-  //         "Content-type": "application/json",
-  //       },
-  //       body: JSON.stringify(reqBody),
-  //       method: "post",
-  //     })
-  //       .then((response) => Promise.all([response.json(), response.headers]))
-  //       .then(([body, headers]) => {
-  //         setJwt(body["token"]);
-  //       });
-  //   }
-  // });
-
-  useEffect(() => {
-    console.log(jwt);
-  }, [jwt]);
-
   return (
     <Routes>
-      <Route path="main" element={<Mainpage />} />
-      <Route path="/" element={<Login />} />
+      <Route
+        path="/*"
+        element={
+          <PrivateRoute>
+            <Mainpage />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
--- front-end/src/login/index.js
+++ front-end/src/login/index.js
@@ -1,15 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocalState } from "../util/useLocalStorage";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [jwt, setJwt] = useLocalState("", "jwt");
+
+  function sendLoginRequest() {
+    if (!jwt) {
+      const reqBody = {
+        email: email,
+        password: password,
+      };
+
+      fetch("http://localhost:8080/api/v1/auth/authenticate", {
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(reqBody),
+        method: "post",
+      })
+        .then((response) => {
+          if (response.status === 200)
+            return Promise.all([response.json(), response.headers]);
+          else return Promise.reject("invalid login attempt");
+        })
+        .then(([body, headers]) => {
+          setJwt(body["token"]);
+          window.location.href = "main";
+        })
+        .catch((message) => {
+          alert(message);
+        });
+    }
+  }
+
   return (
     <>
       <div>
         <label htmlFor="username">Email</label>
-        <input type="email" id="username" />
+        <input
+          type="email"
+          id="username"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <div>
+        <button id="submit" type="button" onClick={() => sendLoginRequest()}>
+          Login
+        </button>
       </div>
     </>
   );
